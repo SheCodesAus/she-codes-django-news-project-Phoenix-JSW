362ee073f7a4707ee46a2a42e59426e935ad7593
Phoenix-JSW <jswylds@gmail.com>
2022-02-12 09:34:24 +0800

    added news views

--- she_codes_news/users/forms.py
+++ she_codes_news/users/forms.py
@@ -7,10 +7,11 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = CustomUser
-        fields = ['username', 'email']
+        fields = ['username', 'email', 'bio', 'location', 'avatar']
+    
 
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = CustomUser
-        fields = ['username', 'email']
+        fields = ['username', 'email', 'bio', 'location', 'avatar']
--- she_codes_news/users/models.py
+++ she_codes_news/users/models.py
@@ -1,7 +1,12 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
+
 
 class CustomUser(AbstractUser):
-    pass
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=200, blank=True)
+    avatar = models.ImageField(null=True, blank=True)
 
-def __str__(self):
-    return self.username
+    def __str__(self):
+        return self.username
