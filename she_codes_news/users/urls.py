ef0d8153d8a94375c309b6798b894bf0426ecc09
Phoenix-JSW <jswylds@gmail.com>
2022-02-12 10:09:40 +0800

    profile page created

--- she_codes_news/users/urls.py
+++ she_codes_news/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import CreateAccountView
+from .views import CreateAccountView, profile
 
 app_name = 'users'
 
 urlpatterns = [
-    path('create-account/', CreateAccountView.as_view(),
-name='createAccount'),
+    path('create-account', CreateAccountView.as_view(), name='createAccount'),
+    path('profile', profile, name='profile')
 ]
--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -3,8 +3,13 @@ from django.views.generic.edit import CreateView
 from django.views import generic
 from .models import CustomUser
 from .forms import CustomUserCreationForm
+from django.shortcuts import render
 
 class CreateAccountView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'users/createAccount.html'
+
+def profile(request):
+    user=request.user
+    return render(request,'users/userProfile.html',{'user':user})
