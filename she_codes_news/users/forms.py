6aa37ef2c71b1e5d7c045b3ec8903eae910d6b56
Phoenix-JSW <jswylds@gmail.com>
2022-02-15 19:41:40 +0800

    Update forms.py

--- she_codes_news/users/forms.py
+++ she_codes_news/users/forms.py
@@ -1,5 +1,3 @@
-from dataclasses import field
-from tkinter import Label
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser, Profile
